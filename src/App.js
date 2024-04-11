b2422163b47f397d2f791aff599f42f3c2cd4ae0
chikephils <chikephils@gmail.com>
2024-04-11 23:01:52 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import ScrollToTop from "./ScrollToTop";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Education from "./components/Education/Education";
@@ -10,19 +11,22 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className={styles.container}>
-      <Navbar />
-      <main className={styles.content}>
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contacts" element={<Contact />} />
-        </Routes>
-      </main>
-    </div>
+    <>
+      <div className={styles.container}>
+        <Navbar />
+        <ScrollToTop />
+        <main className={styles.content}>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contacts" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </>
   );
 }
 
